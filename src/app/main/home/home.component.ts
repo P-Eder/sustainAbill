98d55b4e0d5f818978fdbfc777aa3e4734306692
P-Eder <patrick_eder@online.de>
2020-02-23 20:42:35 +0100

    sum of reciepts is automatically updated

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -8,54 +8,64 @@ import { Component, OnInit } from "@angular/core";
 export class HomeComponent implements OnInit {
 
   listOfReciepts = [{
-    "date": "14 JAN",
-    "shop": "Bakery",
-    "price": "12,30 EUR",
-    "items": ["Coffe 1", "Coffe 2"]
+    date: "14 JAN",
+    shop: "Bakery",
+    price: "12,30 EUR",
+    items: ["Coffe 1", "Coffe 2"]
   },
   {
-    "date": "14 JAN",
-    "shop": "IBM Coffee",
-    "price": "1,50 EUR",
-    "items": ["Coffe 1", "Coffe 2"]
+    date: "14 JAN",
+    shop: "IBM Coffee",
+    price: "1,50 EUR",
+    items: ["Coffe 1", "Coffe 2"]
   },
   {
-    "date": "18 JAN",
-    "shop": "IBM Coffee",
-    "price": "1,50 EUR",
-    "items": ["Coffe 1", "Coffe 2"]
+    date: "18 JAN",
+    shop: "IBM Coffee",
+    price: "1,50 EUR",
+    items: ["Coffe 1", "Coffe 2"]
   },
   {
-    "date": "23 JAN",
-    "shop": "IBM Coffee",
-    "price": "1,50 EUR",
-    "items": ["Coffe 1", "Coffe 2"]
+    date: "23 JAN",
+    shop: "IBM Coffee",
+    price: "1,50 EUR",
+    items: ["Coffe 1", "Coffe 2"]
   },
   {
-    "date": "18 FEB",
-    "shop": "IBM Coffee",
-    "price": "1,50 EUR",
-    "items": ["Coffe 1", "Coffe 2"]
+    date: "18 FEB",
+    shop: "IBM Coffee",
+    price: "1,50 EUR",
+    items: ["Coffe 1", "Coffe 2"]
   },
   {
-    "date": "19 FEB",
-    "shop": "REWE",
-    "price": "15,95 EUR",
-    "items": ["Coffe 1", "Coffe 2"]
+    date: "19 FEB",
+    shop: "REWE",
+    price: "15, 95 EUR",
+    items: ["Coffe 1", "Coffe 2"]
   },
   {
-    "date": "19 FEB",
-    "shop": "IBM Coffee",
-    "price": "1,50 EUR",
-    "items": ["Coffe 1", "Coffe 2"]
+    date: "19 FEB",
+    shop: "IBM Coffee",
+    price: "1,50 EUR",
+    items: ["Coffe 1", "Coffe 2"]
   },
   {
-    "date": "18 FEB",
-    "shop": "Irish Pub",
-    "price": "12,00 EUR",
-    "items": ["Coffe 1", "Coffe 2"]
+    date: "18 FEB",
+    shop: "Irish Pub",
+    price: "12,00 EUR",
+    items: ["Coffe 1", "Coffe 2"]
+  },
+  {
+    date: "18 FEB",
+    shop: "Irish Pub",
+    price: "12,00 EUR",
+    items: ["Coffe 1", "Coffe 2"]
   }
   ];
+
+  sumTotalReciepts = this.listOfReciepts.length;
+
+
 
   constructor() { }
 
