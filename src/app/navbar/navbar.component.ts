ffba60cb6672502554b40f68430cfa85402bbf96
P-Eder <patrick_eder@online.de>
2020-02-25 23:37:00 +0100

    changed design - not done yet

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,18 +7,10 @@ declare var M: any;
   styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit, AfterViewInit {
-  hideNav = false;
   constructor() { }
 
   ngOnInit() { }
 
   ngAfterViewInit() {
-    const optionsSidenav = { edge: "right" };
-    const elemsSidenav = document.querySelectorAll(".sidenav");
-    const instancesSidenav = M.Sidenav.init(elemsSidenav, optionsSidenav);
-
-    const optionModalLogin = {};
-    const elemsModalLogin = document.querySelectorAll(".modal");
-    const instancesModalLogin = M.Modal.init(elemsModalLogin, optionModalLogin);
   }
 }
